a60feda54da1237b9183071f88243e8f3cd59a77
Erdemstar <erdem-yildiz@windowslive.com>
2022-11-24 20:34:10 +0300

    App is working well but it's not finished. Basicly it takes parametres from users then read all file's sentence and compare againts rules.json

---
+++ src/seek4treasure.py
@@ -0,0 +1,32 @@
+from core import Core
+from datetime import datetime
+import os,sys
+
+root_path = os.path.dirname(os.path.abspath(__file__)) + "/../"
+rule_path = root_path + "/resource/rules.json"
+folder_path = sys.argv[1]
+output_path = folder_path + str(datetime.today()).replace(" ","-") + "-result.json"
+
+def help():
+    print ("[!] There is an error while giving arguments")
+    print ("python main.py /Users/tom/Desktop/Test/")
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        help()
+        sys.exit()
+    c = Core(folder_path,rule_path,output_path)
+    files = c.allFilesinFolder()
+    rules = c.readRulesFromJson()
+    
+    print ("Seek4Treasure is started")
+    
+    #Walk in all files
+    for file in files:
+        c.readFile(file)    
+
+    #After everything finish create result
+    c.create_output()
+
+    print ("Everything is okay. Result path is " + output_path)
+    

8b33baea9e430d72cc9be103f18852a5ce1d8709
Erdemstar <erdem-yildiz@windowslive.com>
2022-11-24 20:57:16 +0300

    Updated a few lines

--- src/seek4treasure.py
+++ src/seek4treasure.py
@@ -1,20 +1,33 @@
+#!/usr/bin/python3
+
 from core import Core
 from datetime import datetime
 import os,sys
 
 root_path = os.path.dirname(os.path.abspath(__file__)) + "/../"
 rule_path = root_path + "/resource/rules.json"
-folder_path = sys.argv[1]
-output_path = folder_path + str(datetime.today()).replace(" ","-") + "-result.json"
+
+folder_path = ""
+output_path = ""
+
+def control():
+    if (len(sys.argv) != 2):
+        help()
+        sys.exit()
+    else:
+        global folder_path
+        folder_path = sys.argv[1]
+
+        global output_path
+        output_path = folder_path + str(datetime.today()).replace(" ","-") + "-result.json"
+
 
 def help():
     print ("[!] There is an error while giving arguments")
     print ("python main.py /Users/tom/Desktop/Test/")
 
 if __name__ == '__main__':
-    if (len(sys.argv) != 2):
-        help()
-        sys.exit()
+    control()
     c = Core(folder_path,rule_path,output_path)
     files = c.allFilesinFolder()
     rules = c.readRulesFromJson()
